21c42ebc798e273ffffc70e82082cfa1671ae7ef
Daniela <danscarballo@gmail.com>
2023-05-08 17:17:03 -0300

    Inicio de proyecto

---
+++ Paginado.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+export const Paginado = () => {
+
+    const [pokemons, setPokemons] = useState([]);
+    const [page, setPage] = useState(0);
+
+    useEffect(() => {
+        fetch()
+        
+    })
+
+
+  return (
+    <div>Paginado</div>
+  )
+}
