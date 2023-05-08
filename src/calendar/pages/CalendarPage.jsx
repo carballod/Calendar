c559267cb2a68a331bb736652781716b1dd16126
Daniela <danscarballo@gmail.com>
2023-05-08 20:55:54 -0300

    Agrega react-big-calendar, personaliza el idioma y el cuadro de evento

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,56 @@
+import { Calendar } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
+import { addHours } from "date-fns";
+import { Navbar, CalendarEvent } from "../";
+import { localizer, getMessagesES } from "../../helpers";
+
+
+const events = [{
+  title: 'Cumpleaños',
+  notes: 'Comprar pastel',
+  start: new Date(),
+  end: addHours( new Date(), 2 ),
+  bgcolor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Daniela'
+  }
+}]
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+    }
+
+    return {
+      style
+    }
+  }
+
   return (
-    <div>CalendarPage</div>
-  )
-}
+    <>
+      <Navbar />
+
+      <Calendar
+        culture="es"
+        localizer={ localizer }
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+        messages={ getMessagesES() }
+        eventPropGetter={ eventStyleGetter }
+        components={{
+          event: CalendarEvent
+        }}
+      />
+    </>
+  );
+};
