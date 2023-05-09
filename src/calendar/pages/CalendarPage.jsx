89f20217f6bec0602bc58854cdf7070f0f37abcc
Daniela <danscarballo@gmail.com>
2023-05-09 04:44:02 -0300

    Crea un Modal para añadir eventos, agrega sus contenidos estilos e idioma

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,8 +2,9 @@ import { Calendar } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 import { addHours } from "date-fns";
-import { Navbar, CalendarEvent } from "../";
+import { Navbar, CalendarEvent, CalendarModal } from "../";
 import { localizer, getMessagesES } from "../../helpers";
+import { useState } from "react";
 
 
 const events = [{
@@ -20,6 +21,8 @@ const events = [{
 
 export const CalendarPage = () => {
 
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week' );
+
   const eventStyleGetter = ( event, start, end, isSelected ) => {
 
     const style = {
@@ -34,6 +37,18 @@ export const CalendarPage = () => {
     }
   }
 
+  const onDoubleClick = ( event ) => {
+    console.log({ doubleClick: event });
+  }
+
+  const onSelect = ( event ) => {
+    console.log({ click: event });
+  }
+
+  const onViewChanged = ( event ) => {
+    localStorage.setItem('lastView', event );
+  }
+
   return (
     <>
       <Navbar />
@@ -42,6 +57,7 @@ export const CalendarPage = () => {
         culture="es"
         localizer={ localizer }
         events={ events }
+        defaultView={ lastView }
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc( 100vh - 80px )' }}
@@ -50,7 +66,13 @@ export const CalendarPage = () => {
         components={{
           event: CalendarEvent
         }}
+        onDoubleClickEvent={ onDoubleClick }
+        onSelectEvent={ onSelect }
+        onView={ onViewChanged }
       />
+
+      <CalendarModal />
+
     </>
   );
 };
