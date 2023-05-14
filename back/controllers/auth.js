be46998b589392df41fd5ec179e4c3324b62dcc3
Daniela <danscarballo@gmail.com>
2023-05-14 09:17:13 -0300

    Crear Json Web Token

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require("express");
 const bcrypt = require("bcryptjs");
 const Usuario = require("../models/Usuario");
+const { generarJTWT } = require("../helpers/jwt");
 
 const crearUsuario = async (req, res = response) => {
   const { email, password } = req.body;
@@ -21,11 +22,14 @@ const crearUsuario = async (req, res = response) => {
     usuario.password = bcrypt.hashSync(password, salt);
 
     await usuario.save();
+    // Generar el JWT
+    const token = await generarJTWT(usuario.id, usuario.name)
 
     res.status(201).json({
       ok: true,
       uid: usuario.id,
       name: usuario.name,
+      token,
     });
 
   } catch (error) {
@@ -37,15 +41,46 @@ const crearUsuario = async (req, res = response) => {
   }
 };
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
   const { email, password } = req.body;
 
-  res.json({
-    ok: true,
-    msg: "login",
-    email,
-    password,
-  });
+  try {
+
+    const usuario = await Usuario.findOne({ email });
+    if ( !usuario ) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Un usuario existe con ese email",
+      });
+    }
+
+    // Confirmar los passwords
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+    if ( !validPassword ) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Password incorrecto",
+      });
+    }
+
+    // Generar el JWT
+    const token = await generarJTWT(usuario.id, usuario.name)
+
+    res.json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+      token,
+    });
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
+
 };
 
 const revalidarToken = (req, res = response) => {
