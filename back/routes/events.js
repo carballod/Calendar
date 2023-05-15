f0fd7351bbc20ebe7cfb03572fa006bda6bf5576
Daniela <danscarballo@gmail.com>
2023-05-15 06:19:34 -0300

    Validar campos necesarios

--- back/routes/events.js
+++ back/routes/events.js
@@ -6,6 +6,8 @@
 const { Router } = require("express");
 const { validarJWT } = require("../middlewares/validar-jwt");
 const { getEvento, crearEvento, actualizarEvento, eliminarEvento } = require("../controllers/events");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
 
@@ -14,7 +16,15 @@ router.use(validarJWT);
 
 router.get("/", getEvento);
 
-router.post("/", crearEvento);
+router.post(
+    "/",
+    [
+        check("title", "El título es obligatorio").not().isEmpty(),
+        check("start", "Fecha de inicio es obligatoria").isDate(), // start 2020-07-12
+        check("end", "Fecha de finalización es obligatoria").isDate(), // end 2020-07-12
+        validarCampos
+    ],
+    crearEvento);
 
 router.put("/:id", actualizarEvento );
 
